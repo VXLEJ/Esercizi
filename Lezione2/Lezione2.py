8fdfe2597af55e41c7feb3f29ae23fac5b21ccd1
VXLEJ <valerio.gambaw@gmail.com>
2024-04-17 13:53:25 +0200

    File Lezione2.py aggiunto

---
+++ Lezione2/Lezione2.py
@@ -0,0 +1,15 @@
+# Valerio Gamba
+# 17/04/2024
+
+print("hello world")
+
+# 2-3. Personal Message: Use a variable to represent a person’s name
+# and print a message to that person.
+# Your message should be simple, such as
+# “Hello Eric, would you like to learn some Python today?”
+
+# Variabili stringa
+Name: str = "Tyler"
+Message: str = f"Ciao {Name}, ti va di imparare un po di python insieme"
+print(Message)
+
